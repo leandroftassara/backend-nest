592425e36424b7a0575e9b0eb27793a358be0d56
Leandro <leandro.tassara@igti.edu.br>
2024-07-10 08:53:30 -0300

    feat(utils): add generateAccountVerificationToken method

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,11 +1,19 @@
+import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class Utils {
   formatEmail(email: string): string {
     return email.trim().toLowerCase();
   }
+
+  getFirstName = (name: string): string => {
+    const firstName = name.split(' ')[0];
+
+    return firstName.charAt(0).toUpperCase() + firstName.slice(1);
+  };
 
   async encryptPassword(password: string): Promise<string> {
     const generatedSalt = await bcrypt.genSalt(10);
@@ -14,9 +22,7 @@ export class Utils {
     return hashedPassword;
   }
 
-  getFirstName = (name: string): string => {
-    const firstName = name.split(' ')[0];
-
-    return firstName.charAt(0).toUpperCase() + firstName.slice(1);
-  };
+  generateAccountVerificationToken(): string {
+    return crypto.randomBytes(64).toString('hex');
+  }
 }
