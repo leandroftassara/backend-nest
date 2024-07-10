90114cfb8ab096e0ddbc10b2256ad816e4328655
Leandro <leandro.tassara@igti.edu.br>
2024-07-10 08:52:53 -0300

    refactor: shared services

--- src/api/users/create-user/create-user.module.ts
+++ src/api/users/create-user/create-user.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CreateUserService } from './create-user.service';
-import { Mailer } from 'src/shared/mailer';
 import { UsersRepository } from '../users.repository';
 import { PrismaService } from 'src/shared/prisma.service';
 import { Utils } from 'src/shared/utils';
+import { MailerService } from 'src/shared/mailer.service';
 
 @Module({
-  providers: [CreateUserService, Utils, Mailer, UsersRepository, PrismaService],
+  providers: [
+    CreateUserService,
+    Utils,
+    MailerService,
+    UsersRepository,
+    PrismaService,
+  ],
   exports: [CreateUserService],
 })
 export class CreateUserModule {}
